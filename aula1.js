1c9b94517d76b13dc1dbd1a18e5da35e304448dd
Kailani-Cortez <kailanicortesbm@gmail.com>
2022-11-25 10:18:57 -0300

    Versão 2 - Node.js - aula1

--- aula1.js
+++ aula1.js
@@ -1,5 +1,8 @@
 var http = require('http');
 http.createServer(function (resq, res){
-    res.write("Brasil 4x2 \n");
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write("Brasil 2x0 \n");
+
+    res.write(Date().substring(16,24)+'<br>');
     res.end("Bom dia!");
 }).listen(3000);
