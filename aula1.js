2179ef7df7e62374ca2a5db83f5b075e8b98173d
Kailani-Cortez <kailanicortesbm@gmail.com>
2022-11-25 10:45:15 -0300

    Versão 3 - Node.js - aula1.js/biblioteca.js

--- aula1.js
+++ aula1.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var diasm = require('./biblioteca');
+
 http.createServer(function (resq, res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.write("Brasil 2x0 \n");
-
-    res.write(Date().substring(16,24)+'<br>');
+    res.write("Brasil 4x1 Suiça \n");
+    res.write('<p style=color:red;>'+Date().substring(16,24)+'<br>');
+    res.write(diasm.diaDaSemana()+'</p><br>')
     res.end("Bom dia!");
 }).listen(3000);
